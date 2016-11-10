77a94abc66d9cd92ac31248d9fd375db0e528d8c
helioh2 <heliohenrique3@gmail.com>
2016-11-10 18:52:14 -0200

    adicionadas mais algumas funcoes interessantes

--- exemplos6/funcoes-elegantes.rkt
+++ exemplos6/funcoes-elegantes.rkt
@@ -42,3 +42,62 @@
 
 (check-equal? (replace-all 5 10 '(1 2 3 4 5 6 5 3 2 5 3 2 6 5))
               '(1 2 3 4 10 6 10 3 2 10 3 2 6 10))
+
+
+;;----------------------
+
+(define (produto-cartesiano-v1 list1 list2)
+  (local
+    [
+    (define (cria-pares item lista)
+      (map (lambda (item2) (list item item2)) lista))
+    ]
+    (cond [(empty? list1) empty]
+          [else
+           (append (cria-pares (first list1) list2)
+                   (produto-cartesiano-v1 (rest list1) list2))])))
+
+(check-equal? (produto-cartesiano-v1 '("a" "b" "c") '(1 2))
+              '(("a" 1) ("a" 2) ("b" 1) ("b" 2) ("c" 1) ("c" 2)))
+
+
+(define (produto-cartesiano-v2 list1 list2)
+  (for*/list ([i list1]
+             [j list2])
+    (list i j)))
+
+(check-equal? (produto-cartesiano-v2 '("a" "b" "c") '(1 2))
+              '(("a" 1) ("a" 2) ("b" 1) ("b" 2) ("c" 1) ("c" 2)))
+
+
+(define (produto-cartesiano-v3 list1 list2)
+  (cartesian-product list1 list2))
+
+(check-equal? (produto-cartesiano-v3 '("a" "b" "c") '(1 2))
+              '(("a" 1) ("a" 2) ("b" 1) ("b" 2) ("c" 1) ("c" 2)))
+
+
+;-------------------------
+
+(define (todas-combinacoes-string list1 list2)
+  (local
+    [
+     (define (to-string x)
+       (cond [(number? x) (number->string x)]
+             [(symbol? x) (symbol->string x)]
+             [(list? x) (list->string x)]
+             [(boolean? x) (if (false? x) "false" "true")]
+             [else x]))
+     ]
+  (map (lambda (par) (string-append (to-string (first par)) " "
+                                    (to-string (second par))))
+       (produto-cartesiano-v3 list1 list2))))
+
+(check-equal? (todas-combinacoes-string '(1 3) '(2 4))
+              '("1 2" "1 4" "3 2" "3 4"))
+
+;--------------------
+
+(define (soma-produto-cartesiano-e-multiplica list1 list2)
+  (foldr + 0 (map (lambda (par) (+ (first par) (second par)))
+                  (produto-cartesiano-v3 list1 list2))))
